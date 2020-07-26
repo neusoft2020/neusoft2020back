013f93b99eece3151d32430df1b972347cbd7c84
吕海东 <lvhaidong2902@126.com>
2020-07-26 21:03:43 +0800

    001

---
+++ neuoa001/src/main/java/com/neusoft/oa/Neuoa001Application.java
@@ -0,0 +1,13 @@
+package com.neusoft.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neuoa001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neuoa001Application.class, args);
+	}
+
+}
---
+++ neuoa001/src/test/java/com/neusoft/oa/Neuoa001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neusoft.oa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neuoa001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
