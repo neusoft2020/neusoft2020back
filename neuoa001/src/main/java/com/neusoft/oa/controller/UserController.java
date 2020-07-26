f6f28ccfbcf820147fa056d5dd4f9fe0fc5da5dd
吕海东 <lvhaidong2902@126.com>
2020-07-26 21:08:08 +0800

    002

---
+++ neuoa001/src/main/java/com/neusoft/oa/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.neusoft.oa.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	@RequestMapping("/ok")
+	public String ok() {
+		return "ok";
+	}
+
+}
